e0063a08850bea7f31bed3678f5a97676bb3cfa0
Thomas Henderson <tom@mathpunk.net>
2018-04-16 19:20:46 -0500

    Faces

--- src/simplexity/core.clj
+++ src/simplexity/core.clj
@@ -1,16 +1,16 @@
 (ns simplexity.core
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.math.combinatorics :as combo]
+            [clojure.set :as set]
+            [clojure.spec.test.alpha :as test]))
 
 (s/def ::vertex nat-int?)
 (s/def ::vertices (s/coll-of ::vertex :distinct true :min-count 1))
 
-(s/exercise ::vertices)
-
 (defprotocol Simplex
   (dim [s] "The dimension of the simplex, defined as |s|-1")
   (size [s] "The count of the vertex set of the simplex")
-  (faces [s] "The subsets of the vertices in the simplex")
-  )
+  (faces [s] "The subsets of the vertices in the simplex"))
 
 (s/fdef dim
         :args (s/cat :integral-vertices ::vertices)
@@ -24,11 +24,18 @@
         :fn #(= (count (:integral-vertices (:args %)))
                 (:ret %)))
 
+(s/fdef faces
+        :args (s/cat :integral-vertices ::vertices)
+        :ret seq?
+        :fn (s/and #(= (int (Math/pow 2 (count (-> % :args :integral-vertices))))
+                       (count (:ret %)))
+                   #(every? (fn [face] (s/valid? ::simplex %)) (:ret %))))
+
 (extend-type clojure.lang.IPersistentCollection
   Simplex
   (dim [s] (- (count s) 1))
   (size [s] (count s))
-  #_(faces [s] ))
+  (faces [s] (combo/subsets s)))
 
 (s/def ::simplex #(satisfies? Simplex %))
 
--- test/simplexity/core_test.clj
+++ test/simplexity/core_test.clj
@@ -4,8 +4,14 @@
             [clojure.spec.test.alpha :as test]
             [simplexity.core :refer :all]))
 
-(defmacro passes [sym]
-  `(is (:check-passed (->> ~sym test/check test/summarize-results))))
+(defn passing
+  ([sym] (:check-passed (->> sym test/check test/summarize-results)))
+  ([sym n] (:check-passed
+            (test/summarize-results
+             (test/check sym
+                         {:clojure.spec.test.check/opts {:num-tests n}})))))
+
+(def ^:dynamic *short-num-tests* 3)
 
 (deftest is-testing
   (is true))
@@ -19,13 +25,18 @@
   (is (= 0 (dim (simplex [0]))))
   (is (= 1 (dim (simplex [0 1]))))
   (is (= 2 (dim (simplex [0 1 2]))))
-  (passes `simplexity.core/dim))
+  (is (passing `simplexity.core/dim)))
 
 (deftest has-size
   (is (- 1 (size (simplex #{0}))))
   (is (- 3 (size (simplex [1 3 6]))))
-  (passes `simplexity.core/size))
+  (is (passing `simplexity.core/size)))
 
-#_(deftest has-simplicial-facets
-    (let [tetrahedron (simplex [0 3 7 12])]
-      (is (every? #(s/valid? :simplexity.core/simplex %) (faces tetrahedron)))))
+(deftest has-simplicial-faces
+  (let [tetrahedron (simplex [0 3 7 12])]
+    (is (every? #(s/valid? :simplexity.core/simplex %) (faces tetrahedron)))
+    (is (some #{[0 7]} (faces tetrahedron)))
+    (is (some #{[0 3 7 12]} (faces tetrahedron)))
+    (is (some #{[12]} (faces tetrahedron)))
+    (is (some #{[]} (faces tetrahedron)))
+    (is (passing `simplexity.core/faces *short-num-tests*))))
