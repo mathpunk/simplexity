8d9466b78e3f7f4df63c533c5acc677e7aeae0a0
Thomas Henderson <tom@mathpunk.net>
2018-04-19 18:02:32 -0500

    wip: Confirm how ensureAllFaces works

--- src/simplexity/impl/javaplex.clj
+++ src/simplexity/impl/javaplex.clj
@@ -6,7 +6,6 @@
   (:import [edu.stanford.math.plex4.streams.impl ExplicitSimplexStream]
            [edu.stanford.math.plex4.api Plex4]))
 
-
 ;; The New API
 ;; --------------------
 ;; Goal: Support passing homology tests in complex_test.clj
@@ -46,48 +45,10 @@
    (add-vertex 0)
    (add-vertex 1)
    (add-element [0 1])))
-
-
-;; Isolating mutation. ???
-
-(def c
-  (doto (e)
-    (add-vertex 0)
-    (add-vertex 1)
-    (add-vertex 2)
-    (add-element [0 1 2])))
-;; .getSize is 4
-
-(.ensureAllFaces c)
-
-(.getSize c)
-;; .getSize is 7
-
-(def t
-  (doto (e)
-    (add-vertex 0)
-    (add-vertex 1)
-    (add-vertex 2)
-    (add-vertex 3)
-    (add-element [0 1 2])
-    (add-element [1 2 3])
-    (add-element [0 1 3])
-    (add-element [0 3 2])))
-
-(.getSize t)
-;; 8???
-(.ensureAllFaces t)
-(.getSize t)
-;; 14
-;; I guess that's right. Yes. That's right. It isn't a valid complex until that ensureAllFaces has happened.
-
-(betti-numbers-map t)
-#_{0 1, 1 3, 2 4}
-;; what WHAT that doesn't look right at all.
+(check! #{3})
 
 (s/def ::plex #(= edu.stanford.math.plex4.streams.impl.ExplicitSimplexStream
                   (type %)))
-
 
 (s/fdef clutter
         :args (s/cat :simplex :simplexity.simplex/simplex)
@@ -102,23 +63,49 @@
 
 (test/check `clutter)
 
+(defn build-simplex [vertices]
+  (let [simplex (clutter vertices)]
+    (.ensureAllFaces simplex)
+    ;; BUT NO ELEMENTS! so it's three
+    simplex))
+
+(let [simplex (clutter [0 1 2])]
+  (.ensureAllFaces simplex)
+  (.getSize simplex))
+(check! #{3})
+
+(let [simplex (clutter [0 1 2])]
+  (add-element simplex [0 1 2])
+  (.ensureAllFaces simplex)
+  (.getSize simplex))
+(check! #{7})
+
+(let [simplex (clutter [0 1 2])]
+  (add-element simplex [0 1])
+  (add-element simplex [1 2])
+  (add-element simplex [2 0])
+  (.ensureAllFaces simplex)
+  (.getSize simplex))
+(check! #{6})
+
+;; See how this works? You put in some elements, and .ensureAllFaces makes sure that it's a valid complex from that.
+
+#_(.getSize (build-complex [[0 1] [1 2] [2 0]]))
+#_(check! #{6})
+
+#_(.getSize (build-complex [[0 1  2]]))
+#_(check! #{7})
+
+#_(.getSize (build-complex [0 1  2]))
+;; Should it be 7 or should it throw?
 
 
 
 
-
-
-
-(.getSize (clutter [0 1 2 3 4 5]))
 
 
 ;; Works but user-hostile
 ;; --------------------
-(defn empty-simplex [] (ExplicitSimplexStream.))
-
-
-(.getSize (clutter [0 1 2 3]))
-(check! #{4})
 
 #_(defn integral-simplex [vertices]
     (let [s (clutter vertices)]
