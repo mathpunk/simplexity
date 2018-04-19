94b2f6d150ad207a9ce76dc54ef60d395ba2b65a
Thomas Henderson <tom@mathpunk.net>
2018-04-19 17:49:48 -0500

    wip: Improving understanding and coverage of javaplex

--- src/simplexity/impl/javaplex.clj
+++ src/simplexity/impl/javaplex.clj
@@ -1,14 +1,121 @@
 (ns simplexity.impl.javaplex
-  (:require [clojure.math.combinatorics :as combo])
+  (:require [clojure.math.combinatorics :as combo]
+            [simplexity.simplex]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as test])
   (:import [edu.stanford.math.plex4.streams.impl ExplicitSimplexStream]
            [edu.stanford.math.plex4.api Plex4]))
 
-(defn empty-simplex [] (ExplicitSimplexStream.))
+
+;; The New API
+;; --------------------
+;; Goal: Support passing homology tests in complex_test.clj
+
+
+;; Let's start by learning to construct javaplex simplexes.
+
+(defn e [] (ExplicitSimplexStream.))
+
+(.getSize (e))
+(check! #{0})
+
+(.getSize (doto (e)
+            (.addVertex 0)))
+(check! #{1})
+
+(.getSize (doto (e)
+            (.addVertex 0)
+            (.addVertex 1)))
+(check! #{2})
+
+(.getSize
+ (doto (e)
+   (.addVertex 0)
+   (.addVertex 1)
+   (.addElement (int-array [0,1]))))
+(check! #{3}) ;; surprise!
+
+(defn add-vertex [simplex vertex]
+  (.addVertex simplex vertex))
+
+(defn add-element [simplex element]
+  (.addElement simplex (int-array element)))
+
+(.getSize
+ (doto (e)
+   (add-vertex 0)
+   (add-vertex 1)
+   (add-element [0 1])))
+
+
+;; Isolating mutation. ???
+
+(def c
+  (doto (e)
+    (add-vertex 0)
+    (add-vertex 1)
+    (add-vertex 2)
+    (add-element [0 1 2])))
+;; .getSize is 4
+
+(.ensureAllFaces c)
+
+(.getSize c)
+;; .getSize is 7
+
+(def t
+  (doto (e)
+    (add-vertex 0)
+    (add-vertex 1)
+    (add-vertex 2)
+    (add-vertex 3)
+    (add-element [0 1 2])
+    (add-element [1 2 3])
+    (add-element [0 1 3])
+    (add-element [0 3 2])))
+
+(.getSize t)
+;; 8???
+(.ensureAllFaces t)
+(.getSize t)
+;; 14
+;; I guess that's right. Yes. That's right. It isn't a valid complex until that ensureAllFaces has happened.
+
+(betti-numbers-map t)
+#_{0 1, 1 3, 2 4}
+;; what WHAT that doesn't look right at all.
+
+(s/def ::plex #(= edu.stanford.math.plex4.streams.impl.ExplicitSimplexStream
+                  (type %)))
+
+
+(s/fdef clutter
+        :args (s/cat :simplex :simplexity.simplex/simplex)
+        :ret ::plex
+        :fn #(= (count (-> % :args :simplex))
+                (.getSize (:ret %))))
 
 (defn clutter [vertices]
-  (let [e (empty-simplex)]
-    (doall (map #(.addVertex e %) vertices))
-    e))
+  (let [simplex (e)]
+    (doall (map #(.addVertex simplex %) vertices))
+    simplex))
+
+(test/check `clutter)
+
+
+
+
+
+
+
+
+(.getSize (clutter [0 1 2 3 4 5]))
+
+
+;; Works but user-hostile
+;; --------------------
+(defn empty-simplex [] (ExplicitSimplexStream.))
+
 
 (.getSize (clutter [0 1 2 3]))
 (check! #{4})
@@ -110,4 +217,3 @@
 
 ;; (.getSize (e))
 ;; (check! #{0})
-
