93a48c32aeaacc2db31c2ba847156294b6f94232
Thomas Henderson <tom@mathpunk.net>
2018-04-19 19:30:47 -0500

    builder functions for simplexes and complexes

--- src/simplexity/impl/javaplex.clj
+++ src/simplexity/impl/javaplex.clj
@@ -12,7 +12,6 @@
 
 
 ;; Let's start by learning to construct javaplex simplexes.
-
 (defn e [] (ExplicitSimplexStream.))
 
 (.getSize (e))
@@ -61,146 +60,38 @@
     (doall (map #(.addVertex simplex %) vertices))
     simplex))
 
+(require '[clojure.pprint :refer (pprint)])
+
+(map pprint (s/exercise :simplexity.simplex/simplex))
+
 (test/check `clutter)
 
 (defn build-simplex [vertices]
-  (let [simplex (clutter vertices)]
+  (doto (clutter vertices)  ;; The vertices
+    (add-element vertices)  ;; The facet
+    (.ensureAllFaces)))     ;; The elements of the facet. Javaplex uses the other definition of faces
+
+(.getSize (build-simplex [0 1 2]))
+(check! #{7})
+
+(defn build-complex [facets]
+  (let [vertices (set (flatten facets))
+        simplex (clutter vertices)]
+    (doall (map #(add-element simplex %) facets))
     (.ensureAllFaces simplex)
-    ;; BUT NO ELEMENTS! so it's three
     simplex))
 
-(let [simplex (clutter [0 1 2])]
-  (.ensureAllFaces simplex)
-  (.getSize simplex))
-(check! #{3})
-
-(let [simplex (clutter [0 1 2])]
-  (add-element simplex [0 1 2])
-  (.ensureAllFaces simplex)
-  (.getSize simplex))
-(check! #{7})
-
-(let [simplex (clutter [0 1 2])]
-  (add-element simplex [0 1])
-  (add-element simplex [1 2])
-  (add-element simplex [2 0])
-  (.ensureAllFaces simplex)
-  (.getSize simplex))
+(.getSize (build-complex [[0 1] [1 2] [2 0]]))
 (check! #{6})
 
-;; See how this works? You put in some elements, and .ensureAllFaces makes sure that it's a valid complex from that.
-
-#_(.getSize (build-complex [[0 1] [1 2] [2 0]]))
-#_(check! #{6})
-
-#_(.getSize (build-complex [[0 1  2]]))
-#_(check! #{7})
-
-#_(.getSize (build-complex [0 1  2]))
-;; Should it be 7 or should it throw?
-
-
-
-
-
-
-;; Works but user-hostile
-;; --------------------
-
-#_(defn integral-simplex [vertices]
-    (let [s (clutter vertices)]
-      (.ensureAllFaces s)
-      (.finalizeStream s)
-      s))
-
-(defn integral-simplex [vertices]
-  (let [s (clutter vertices)
-        edges (->> vertices
-                   combo/subsets
-                   (remove #(<= (count %) 1))
-                   (map int-array))]
-    (doall (map #(.addElement s %) edges))
-    s))
-
-(.getSize (integral-simplex [0 1 2]))
+(.getSize (build-complex [[0 1 2]]))
 (check! #{7})
 
-;; checks out ;;;
-
-(def triangle
-  (doto (ExplicitSimplexStream.)
-    (.addVertex 0)
-    (.addVertex 1)
-    (.addVertex 2)
-    (.addElement (int-array [0 1]))
-    (.addElement (int-array [1 2]))
-    (.addElement (int-array [2 0]))
-    (.finalizeStream)))
-
-(def solid-triangle
-  (doto (ExplicitSimplexStream.)
-    (.addVertex 0)
-    (.addVertex 1)
-    (.addVertex 2)
-    (.addElement (int-array [0 1]))
-    (.addElement (int-array [1 2]))
-    (.addElement (int-array [2 0]))
-    (.addElement (int-array [0 1 2]))
-    (.finalizeStream)))
-
-(def edge-glued-triangles
-  (doto (ExplicitSimplexStream.)
-    (.addVertex 0)
-    (.addVertex 1)
-    (.addVertex 2)
-    (.addVertex 3)
-    (.addElement (int-array [0 1]))
-    (.addElement (int-array [1 2]))
-    (.addElement (int-array [2 0]))
-    (.addElement (int-array [1 3]))
-    (.addElement (int-array [2 3]))
-    (.finalizeStream)))
 
 
 
 
-(def ^:dynamic *max-dimension* 5)
-
-(def ^:dynamic *simplicial-algorithm*
-  (Plex4/getDefaultSimplicialAlgorithm *max-dimension*))
-
-(defn barcodes [complex]
-  ;; TODO: ensure 'finalized'?
-  (.computeIntervals *simplicial-algorithm* complex))
-
-(defn betti-numbers-map
-  ([complex]
-   (betti-numbers-map complex 0.0))
-  ([complex filtration]
-   (into {} (.getBettiNumbersMap (barcodes complex) filtration))))
-
-(defn connected-components [s]
-  (get (betti-numbers-map s) 0))
-
-(defn connected? [s]
-  (= (connected-components s) 1))
-
-((betti-numbers-map edge-glued-triangles) 0)
-(check! #{1})
-((betti-numbers-map edge-glued-triangles) 1)
-(check! #{2})
 
 
-
-;; Okay. That all checks out, it's just that the api sucks.
-
-;; BETTER API MAYBE
-
-;; ;; The Empty Simplex: Properties unknown
-;; (defn e [] (ExplicitSimplexStream.))
-
-;; (defn algorithm-by-dimension [d]
-;;   (Plex4/getDefaultSimplicialAlgorithm d))
-
-;; (.getSize (e))
-;; (check! #{0})
+(.getSize (build-complex [0 1 2]))
+(check! #{7})
